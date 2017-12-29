491edde0558fb2c2731e1fb5ffdf79b10f4d596a
AyushR1 <ayushrathore12501@gmail.com>
2017-12-29 14:41:03 +0530

    qpnp-smbcharger: Relax Default ICL for HVDCP
    This would allow to use 2.4A of Current on 5V properly.
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/power/qpnp-smbcharger.c
+++ drivers/power/qpnp-smbcharger.c
@@ -439,7 +439,7 @@ module_param_named(
 	int, S_IRUSR | S_IWUSR
 );
 
-static int smbchg_default_hvdcp_icl_ma = 1800;
+static int smbchg_default_hvdcp_icl_ma = 2100;
 module_param_named(
 	default_hvdcp_icl_ma, smbchg_default_hvdcp_icl_ma,
 	int, S_IRUSR | S_IWUSR
