86ff2fd7e712e276d358c6d2e00132326d1e00ee
AyushR1 <ayushrathore12501@gmail.com>
2018-01-21 13:38:11 +0530

    Revert "Remove Speaker gain."
    This reverts commit c958cbf232606fb31c7d305745a8f9243f875ed6.
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- sound/soc/codecs/sound_control.c
+++ sound/soc/codecs/sound_control.c
@@ -13,6 +13,7 @@
 #define SOUND_CONTROL_MINOR_VERSION 1
 
 extern void update_headphones_volume_boost(int vol_boost);
+extern void update_speaker_gain(int vol_boost);
 extern void update_mic_gain(int vol_boost);
 extern void update_earpiece_gain(int vol_boost);
 
@@ -20,6 +21,12 @@ extern void update_earpiece_gain(int vol_boost);
 int headphones_boost = 0;
 int headphones_boost_limit = 25;
 int headphones_boost_limit_min = -25;
+
+//Speakers
+
+int speaker_boost = 0;
+int speaker_boost_limit = 25;
+ int speaker_boost_limit_min = -25;
 
 //Micrphone
 int mic_boost = 5;
@@ -55,6 +62,35 @@ static ssize_t headphones_boost_store(struct device *dev,
 
 		headphones_boost = new_val;
 		update_headphones_volume_boost(headphones_boost);
+	}
+
+	return size;
+}
+
+static ssize_t speaker_boost_show(struct device *dev,
+		struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%d\n", speaker_boost);
+}
+
+static ssize_t speaker_boost_store(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	int new_val;
+
+	sscanf(buf, "%d", &new_val);
+
+	if (new_val != speaker_boost) {
+		if (new_val <= speaker_boost_limit_min)
+			new_val = speaker_boost_limit_min;
+
+		else if (new_val >= speaker_boost_limit)
+			new_val = speaker_boost_limit;
+
+		pr_info("New speaker_boost: %d\n", new_val);
+
+		speaker_boost = new_val;
+		update_speaker_gain(speaker_boost);
 	}
 
 	return size;
@@ -119,6 +155,7 @@ static ssize_t earpiece_boost_store(struct device *dev,
 }
 
 static DEVICE_ATTR(volume_boost, 0664, headphones_boost_show,headphones_boost_store);
+static DEVICE_ATTR(speaker_boost, 0664, speaker_boost_show, speaker_boost_store);
 static DEVICE_ATTR(mic_boost, 0664, mic_boost_show, mic_boost_store);
 static DEVICE_ATTR(earpiece_boost, 0664, earpiece_boost_show, earpiece_boost_store);
 
@@ -126,6 +163,7 @@ static DEVICE_ATTR(earpiece_boost, 0664, earpiece_boost_show, earpiece_boost_sto
 static struct attribute *soundcontrol_attributes[] =
 {
 	&dev_attr_volume_boost.attr,
+	&dev_attr_speaker_boost.attr,
 	&dev_attr_mic_boost.attr,
 	&dev_attr_earpiece_boost.attr,
 	NULL
