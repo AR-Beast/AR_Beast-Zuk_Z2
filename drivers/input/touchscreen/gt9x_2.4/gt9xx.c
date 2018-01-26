61eb88af6e50769102ae3b72ab841c77f529beff
AyushR1 <ayushrathore12501@gmail.com>
2018-01-26 20:48:37 +0530

    gt9xx.c: Fix to few arguments to function.
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/input/touchscreen/gt9x_2.4/gt9xx.c
+++ drivers/input/touchscreen/gt9x_2.4/gt9xx.c
@@ -1782,7 +1782,7 @@ static s8 gtp_request_input_dev(struct goodix_ts_data *ts)
 
 	ts->input_dev->evbit[0] = BIT_MASK(EV_SYN) | BIT_MASK(EV_KEY) | BIT_MASK(EV_ABS) ;
 #if GTP_ICS_SLOT_REPORT
-	input_mt_init_slots(ts->input_dev, 16);	 // in case of "out of memory"
+	input_mt_init_slots(ts->input_dev, 16, 0);	 // in case of "out of memory"
 #else
 	ts->input_dev->keybit[BIT_WORD(BTN_TOUCH)] = BIT_MASK(BTN_TOUCH);
 #endif
