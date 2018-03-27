51bb33444bb868a9480719ccf78d2f2d241b8c29
AyushR1 <ayushrathore12501@gmail.com>
2018-03-27 16:18:21 +0530

    drivers: staging: lmk checkout to kernel/common
    https://android.googlesource.com/kernel/common/+/android-3.18/drivers/staging/android/lowmemorykiller.c
    
    Signed-off-by: AyushR1 <ayushrathore12501@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -29,9 +29,7 @@
  * GNU General Public License for more details.
  *
  */
-
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
-
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/mm.h>
@@ -40,7 +38,8 @@
 #include <linux/swap.h>
 #include <linux/rcupdate.h>
 #include <linux/notifier.h>
-
+#define CREATE_TRACE_POINTS
+#include "trace/lowmemorykiller.h"
 static uint32_t lowmem_debug_level = 1;
 static short lowmem_adj[6] = {
 	0,
@@ -56,15 +55,12 @@ static int lowmem_minfree[6] = {
 	16 * 1024,	/* 64MB */
 };
 static int lowmem_minfree_size = 4;
-
 static unsigned long lowmem_deathpending_timeout;
-
 #define lowmem_print(level, x...)			\
 	do {						\
 		if (lowmem_debug_level >= (level))	\
 			pr_info(x);			\
 	} while (0)
-
 static unsigned long lowmem_count(struct shrinker *s,
 				  struct shrink_control *sc)
 {
@@ -73,7 +69,6 @@ static unsigned long lowmem_count(struct shrinker *s,
 		global_page_state(NR_INACTIVE_ANON) +
 		global_page_state(NR_INACTIVE_FILE);
 }
-
 static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 {
 	struct task_struct *tsk;
@@ -91,7 +86,6 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 						global_page_state(NR_SHMEM) -
 						global_page_state(NR_UNEVICTABLE) -
 						total_swapcache_pages();
-
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
 	if (lowmem_minfree_size < array_size)
@@ -103,31 +97,24 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			break;
 		}
 	}
-
 	lowmem_print(3, "lowmem_scan %lu, %x, ofree %d %d, ma %hd\n",
 			sc->nr_to_scan, sc->gfp_mask, other_free,
 			other_file, min_score_adj);
-
 	if (min_score_adj == OOM_SCORE_ADJ_MAX + 1) {
 		lowmem_print(5, "lowmem_scan %lu, %x, return 0\n",
 			     sc->nr_to_scan, sc->gfp_mask);
 		return 0;
 	}
-
 	selected_oom_score_adj = min_score_adj;
-
 	rcu_read_lock();
 	for_each_process(tsk) {
 		struct task_struct *p;
 		short oom_score_adj;
-
 		if (tsk->flags & PF_KTHREAD)
 			continue;
-
 		p = find_lock_task_mm(tsk);
 		if (!p)
 			continue;
-
 		if (test_tsk_thread_flag(p, TIF_MEMDIE) &&
 		    time_before_eq(jiffies, lowmem_deathpending_timeout)) {
 			task_unlock(p);
@@ -157,45 +144,45 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 			     p->comm, p->pid, oom_score_adj, tasksize);
 	}
 	if (selected) {
+		long cache_size = other_file * (long)(PAGE_SIZE / 1024);
+		long cache_limit = minfree * (long)(PAGE_SIZE / 1024);
+		long free = other_free * (long)(PAGE_SIZE / 1024);
+		trace_lowmemory_kill(selected, cache_size, cache_limit, free);
 		lowmem_print(1, "Killing '%s' (%d), adj %hd,\n" \
 				"   to free %ldkB on behalf of '%s' (%d) because\n" \
-				"   cache %ldkB is below limit %ldkB for oom_score_adj %hd\n",
+				"   cache %ldkB is below limit %ldkB for oom_score_adj %hd\n" \
+				"   Free memory is %ldkB above reserved\n",
 			     selected->comm, selected->pid,
 			     selected_oom_score_adj,
 			     selected_tasksize * (long)(PAGE_SIZE / 1024),
 			     current->comm, current->pid,
-			     other_file * (long)(PAGE_SIZE / 1024),
-			     minfree * (long)(PAGE_SIZE / 1024),
-			     min_score_adj);
+			     cache_size, cache_limit,
+			     min_score_adj,
+			     free);
 		lowmem_deathpending_timeout = jiffies + HZ;
 		set_tsk_thread_flag(selected, TIF_MEMDIE);
 		send_sig(SIGKILL, selected, 0);
 		rem += selected_tasksize;
 	}
-
 	lowmem_print(4, "lowmem_scan %lu, %x, return %lu\n",
 		     sc->nr_to_scan, sc->gfp_mask, rem);
 	rcu_read_unlock();
 	return rem;
 }
-
 static struct shrinker lowmem_shrinker = {
 	.scan_objects = lowmem_scan,
 	.count_objects = lowmem_count,
 	.seeks = DEFAULT_SEEKS * 16
 };
-
 static int __init lowmem_init(void)
 {
 	register_shrinker(&lowmem_shrinker);
 	return 0;
 }
-
 static void __exit lowmem_exit(void)
 {
 	unregister_shrinker(&lowmem_shrinker);
 }
-
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_AUTODETECT_OOM_ADJ_VALUES
 static short lowmem_oom_adj_to_oom_score_adj(short oom_adj)
 {
@@ -204,28 +191,22 @@ static short lowmem_oom_adj_to_oom_score_adj(short oom_adj)
 	else
 		return (oom_adj * OOM_SCORE_ADJ_MAX) / -OOM_DISABLE;
 }
-
 static void lowmem_autodetect_oom_adj_values(void)
 {
 	int i;
 	short oom_adj;
 	short oom_score_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
-
 	if (lowmem_adj_size < array_size)
 		array_size = lowmem_adj_size;
-
 	if (array_size <= 0)
 		return;
-
 	oom_adj = lowmem_adj[array_size - 1];
 	if (oom_adj > OOM_ADJUST_MAX)
 		return;
-
 	oom_score_adj = lowmem_oom_adj_to_oom_score_adj(oom_adj);
 	if (oom_score_adj <= OOM_ADJUST_MAX)
 		return;
-
 	lowmem_print(1, "lowmem_shrink: convert oom_adj to oom_score_adj:\n");
 	for (i = 0; i < array_size; i++) {
 		oom_adj = lowmem_adj[i];
@@ -235,35 +216,27 @@ static void lowmem_autodetect_oom_adj_values(void)
 			     oom_adj, oom_score_adj);
 	}
 }
-
 static int lowmem_adj_array_set(const char *val, const struct kernel_param *kp)
 {
 	int ret;
-
 	ret = param_array_ops.set(val, kp);
-
 	/* HACK: Autodetect oom_adj values in lowmem_adj array */
 	lowmem_autodetect_oom_adj_values();
-
 	return ret;
 }
-
 static int lowmem_adj_array_get(char *buffer, const struct kernel_param *kp)
 {
 	return param_array_ops.get(buffer, kp);
 }
-
 static void lowmem_adj_array_free(void *arg)
 {
 	param_array_ops.free(arg);
 }
-
 static struct kernel_param_ops lowmem_adj_array_ops = {
 	.set = lowmem_adj_array_set,
 	.get = lowmem_adj_array_get,
 	.free = lowmem_adj_array_free,
 };
-
 static const struct kparam_array __param_arr_adj = {
 	.max = ARRAY_SIZE(lowmem_adj),
 	.num = &lowmem_adj_size,
@@ -272,7 +245,6 @@ static const struct kparam_array __param_arr_adj = {
 	.elem = lowmem_adj,
 };
 #endif
-
 module_param_named(cost, lowmem_shrinker.seeks, int, S_IRUGO | S_IWUSR);
 #ifdef CONFIG_ANDROID_LOW_MEMORY_KILLER_AUTODETECT_OOM_ADJ_VALUES
 module_param_cb(adj, &lowmem_adj_array_ops,
@@ -285,10 +257,6 @@ module_param_array_named(adj, lowmem_adj, short, &lowmem_adj_size,
 module_param_array_named(minfree, lowmem_minfree, uint, &lowmem_minfree_size,
 			 S_IRUGO | S_IWUSR);
 module_param_named(debug_level, lowmem_debug_level, uint, S_IRUGO | S_IWUSR);
-
 module_init(lowmem_init);
 module_exit(lowmem_exit);
-
 MODULE_LICENSE("GPL");
-
-
